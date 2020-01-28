e29998505ec4dd73cc02fc8463a254e0b3fbb474
AC59852 <austin.caron1@gmail.com>
2020-01-28 15:45:09 -0500

    added socket functionality

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 
+const io = require('socket.io')();
 const port = process.env.PORT || 3030;
 
 // tell express where our static files are (js, images, css etc)
@@ -13,3 +14,16 @@ app.get('/', (req, res) => {
 const server = app.listen(port, () => {
     console.log(`app is running on port ${port}`);
 });
+
+// this is all of the socket io functionality
+io.attach(server);
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+
+    // listen for a disconnect event
+    socket.on('disconnect', function() {
+        console.log("a user disconnected");
+    });
+
+  });
