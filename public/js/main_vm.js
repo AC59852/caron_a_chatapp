85394a2ba1fd011d7aaba7582b22d8362a6aa362
AC59852 <austin.caron1@gmail.com>
2020-01-28 16:28:54 -0500

    added backend functions

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,3 +1,16 @@
 // imports always go first - if we're importing anything
 
 const socket = io();
+
+function setUserId(packet) {
+    console.log(packet);
+}
+
+function runDisconnectMsg(packet) {
+    console.log(packet)
+}
+
+// some event handling -> these events are coming from the server
+socket.addEventListener('connected', setUserId);
+
+socket.addEventListener('user_disconnect', runDisconnectMsg);
