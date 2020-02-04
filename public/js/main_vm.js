821cad82277a8235136509d0529d59a85e62830a
AC59852 <austin.caron1@gmail.com>
2020-02-04 15:14:48 -0500

    adding socket

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -1,16 +1,51 @@
 // imports always go first - if we're importing anything
+import ChatMessage from "./modules/ChatMessage.js";
 
 const socket = io();
 
-function setUserId(packet) {
-    console.log(packet);
+function setUserId({sID, message}) {
+    vm.socketID = sID;
 }
 
 function runDisconnectMsg(packet) {
     console.log(packet)
 }
 
+function appendNewMessage(msg) {
+    vm.messages.push(msg);
+}
+
+// this is our main Vue instance
+const vm = new Vue({
+    data: {
+        socketID: "",
+        messages: [],
+        message: "",
+        nickName: ""
+},
+
+methods: {
+    dispatchMessage() {
+        // emit a message event and send the message to the server
+        console.log('handle send message');
+
+        socket.emit('chat_message', { content: this.message, name: this.nickName || "anonymous" });
+        this.message = "";
+    }
+},
+
+components: {
+    newmessage: ChatMessage
+},
+
+mounted: function() {
+    console.log("mounted");
+}
+}).$mount("#app");
+
 // some event handling -> these events are coming from the server
 socket.addEventListener('connected', setUserId);
 
 socket.addEventListener('user_disconnect', runDisconnectMsg);
+
+socket.addEventListener('new_message', appendNewMessage);
