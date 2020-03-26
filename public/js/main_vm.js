7a3377ec9633830b000874bf77c9b801f5a92c78
AC59852 <austin.caron1@gmail.com>
2020-03-25 22:32:23 -0400

    preventing keyboard push

--- public/js/main_vm.js
+++ public/js/main_vm.js
@@ -121,6 +121,11 @@ const vm = new Vue({
             });
         });
 
+        $('textarea').on('focus', function(e) {
+            e.preventDefault(); e.stopPropagation();
+            window.scrollTo(0,0); //the second 0 marks the Y scroll pos. Setting this to i.e. 100 will push the screen up by 100px. 
+        });
+
 
     },
 
